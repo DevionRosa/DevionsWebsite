dd0e08a693c9c0c936c37fe61af37af4f3c94cec
DevionRosa <devionrosa@gmail.com>
2026-03-20 01:02:16 -0400

    changed fonts and added tech stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import { Typewriter } from 'react-simple-typewriter'
 import Navbar from './components/Navbar'
 import ProjectList from './components/Projects'
@@ -6,7 +6,7 @@ import headshot from './assets/headshot.jpg'
 
 function App() {
   return (
-    <div className="min-h-screen text-white"> {/* Optional: Adds a consistent background */}
+    <div className="min-h-screen text-white font-sans"> {/* Optional: Adds a consistent background */}
       <Navbar />
       
       {/* Main Container: This centers the entire website content */}
@@ -14,9 +14,9 @@ function App() {
         
         {/* Header Section */}
         <div className="w-full md:w-1/2 text-center md:text-left">
-          <h1 className="text-4xl font-bold pb-4">Hello, I'm Devion👋</h1>
+          <h1 className="text-6xl font-bold pb-4">Hello, I'm Devion👋</h1>
         
-          <div className="text-2xl font-semibold text-purple-950">
+          <div className="text-3xl font-semibold text-purple-950">
             I'm a {' '}
             <span>
               <Typewriter
@@ -34,15 +34,35 @@ function App() {
         <hr className="border-t border-purple-950 my-12" />
 
           {/* About Me Section */}
-        <section id="about" className="py-12text-center scroll-mt-20">
-          <h2 className="text-3xl font-bold mb-6">About Me</h2>
-          <div>
-           <ul className="space-y-2">
-            <li className="before:content-['🚀'] before:mr-2">Built a React Portfolio</li>
-            <li className="before:content-['💻'] before:mr-2">Sophomore CS Student</li>
-            <li className="before:content-['🏎️'] before:mr-2">Working on a 2005 Infiniti G35x</li>
-          </ul>
-          </div>
+        <section id="about" className="py-12 text-center scroll-mt-20">
+          <h2 className="text-6xl font-bold mb-12">About Me</h2>
+          
+            <div className="flex space-x-8">
+              <div className="relative flex items-center justify-center w-100 h-100 group">              
+              <div className="absolute inset-0 bg-purple-600 rounded-full blur-xl opacity-60 animate-pulse opacity-80 transition-opacity"></div>
+              <img src={headshot} alt="Devion Rosa" className="relative z-10 w-80 h-80 rounded-full object-cover border-4 border-purple-950 shadow-xl"/>
+              </div>
+              <div className="flex-1">
+                <ul className="space-y-20 text-left">
+
+                  <li className="flex items-start gap-3">
+                    <span className="text-4xl shrink-0">💻</span>
+                    <span className="leading-relaxed">I's a Software Engineer aiming to build systems that don't just execute commands but intelligently adapt to user needs.</span>
+                  </li>
+                  
+                  <li className="flex items-start gap-3">
+                    <span className="text-4xl shrink-0">🎓</span>
+                    <span className="leading-relaxed">Currently pursuring a B.S. in Computer Science (Class of 2027), focusing on the mathematical foundations of Machine Learning and the practicalities of scalable full-stack development.</span>
+                  </li>
+                  
+                  <li className="flex items-start gap-3">
+                    <span className="text-4xl shrink-0">💡</span>
+                    <span className="leading-relaxed">From exploring my first system at age 3 to architecting complex NLP bots today, my journey has been defined by a relentless curiosity.</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          
         </section>
 
         {/* Resume Section
@@ -54,7 +74,7 @@ function App() {
 
         {/* Projects Section */}
         <section id="projects" className="py-12 scroll-mt-20">
-          <h2 className="text-3xl font-bold text-center mb-10">Featured Projects</h2>
+          <h2 className="text-6xl font-bold mb-12">Featured Projects</h2>
           <ProjectList />
         </section>
 
@@ -62,7 +82,7 @@ function App() {
 
         {/* Contact Section */}
         <section id="contact" className="py-12 flex flex-col items-center gap-4 scroll-mt-20">
-          <h2 className="text-3xl font-bold mb-2">Get In Touch</h2>
+          <h2 className="text-6xl font-bold mb-12">Get In Touch</h2>
           <a href="mailto:devionrosa@gmail.com" className="text-blue-400 hover:underline text-xl">
             devionrosa@gmail.com
           </a>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,6 +5,7 @@ interface Project {
   description: string;
   link: string;
   img: string;
+  tech: string[];
 }
 
 const projects: { [key: string]: Project } = {
@@ -12,92 +13,117 @@ const projects: { [key: string]: Project } = {
     name: "Bite Buddy",
     description: "BiteBuddy is an AI-powered nutrition planner that reduces food waste by generating personalized recipe ideas from ingredients you already have. It also creates customized meal plans based on your mood, dietary needs, and preferences. I worked as a full-stack developer for this project, optimizing Gemini responses using prompt engineering, and designing interactive and appealing components.",
     link: "https://github.com/mustafa-nom/BiteBuddy",
-    img: new URL("../assets/BiteBuddyimg.png", import.meta.url).href
+    img: new URL("../assets/BiteBuddyimg.png", import.meta.url).href,
+    tech: ["React", "FireBase", "Tailwind CSS", "Gemini API", "Spoonacular API"],
   },
 
   project2: {
     name: "Representative Tracker",
     description: "This project allows users to find their local congressional representatives, analyze voting records/statements, & generate AI-based summaries using Google Gemini. My main role was a backend developer where I focused on fetching legislation and adding it to the database to speed up time.",
     link: "https://github.com/mustafa-nom/RepTracker",
-    img: new URL("../assets/RepTrackerimg.png", import.meta.url).href
+    img: new URL("../assets/RepTrackerimg.png", import.meta.url).href,
+    tech: ["Python", "Flask", "SQLite", "Gemini API", "React", "Congress API"],
   },
 
   project3: {
     name: "YouTube Anti-Recommender",
     description: "This project calls the YouTube API to extract metadata from a video using its URL, and generates antonyms using Google Gemini's API, and then searches YouTube using these new tags to come up with anti-recommendations. My work mainly consisted of fetching metadata and prompt engineering Gemini. We plan to make this a Chrome Extenstion.",
     link: "https://github.com/DevionRosa/YouTubeAntiRecommender",
-    img: new URL("../assets/YouTubeRecimg.png", import.meta.url).href
+    img: new URL("../assets/YouTubeRecimg.png", import.meta.url).href,
+    tech: ["YouTube API", "Gemini API", "Python", "SQLite"],
   },
 
   project4: {
     name: "Discord Chat Bot",
-    description: "When playing unfamiliar or familiar games scouring the wiki to look for specific items takes too much time out of your gameplay. To combat this I developed a discord bot that utilizes web scraping and natural language processing (NKP) to cut down on the time spent viewing the wiki, while also getting the information you need to have a successful playthrough.",
+    description: "When playing unfamiliar or familiar games scouring the wiki to look for specific items takes too much time out of your gameplay. To combat this I developed a discord bot that utilizes web scraping and natural language processing (NLP) to cut down on the time spent viewing the wiki, while also getting the information you need to have a successful playthrough.",
     link: "https://github.com/DevionRosa/Discord-Bot",
-    img: new URL("../assets/DiscordBotimg.png", import.meta.url).href
+    img: new URL("../assets/DiscordBotimg.png", import.meta.url).href,
+    tech: ["Python", "NextCord", "BeautifulSoup", "SentenceTransformer", "Numpy", "Pickle"],
   },
 
   project5: {
     name: "NFL Concussions Analysis",
     description: "Conducted exploratory data analysis (EDA) to visualize trends in NFL concussions and also developed predictive models to assess concussion likelihood and forcast recovery timelines.",
     link: "https://github.com/DevionRosa/Concussion-Injuries",
-    img: new URL("../assets/NFLConcussionsimg.png", import.meta.url).href
+    img: new URL("../assets/NFLConcussionsimg.png", import.meta.url).href,
+    tech: ["Python", "Pandas", "Matplotlib", "Scikit-Learn"],
   },
 
   project6: {
     name: "Voter Registration Analysis",
     description: "Applied time-series and comparative analysis to assess voter spikes, informing historical context and trend forecasting. Also, created geographic visualizations to illustrate regional disparities and highlight population-driven registration patterns.",
     link: "https://github.com/DevionRosa/VotersDataAnalysis",
-    img: new URL("../assets/VoterAnalysisimg.png", import.meta.url).href
+    img: new URL("../assets/VoterAnalysisimg.png", import.meta.url).href,
+    tech: ["Python", "NumPy", "Seaborn", "Plotly"],
   },
 
   project7: {
     name: "Multi-Machine Chatroom",
     description: "Using Java sockets and threads, I created a ChatRoom that was accessable on multiple machines. Some of the features include personalized text colors, 6 or more users, and under 100ms latency.",
     link: "https://github.com/DevionRosa/Multi-Machine-Console-Chatroom",
-    img: new URL("../assets/ChatRoomimg.png", import.meta.url).href
+    img: new URL("../assets/ChatRoomimg.png", import.meta.url).href,
+    tech: ["Java", "Socket Programming", "Multi-threading"],
   },
 
   project8: {
     name: "Flappy Bird",
     description: "A python application replicating the famous Flappy Bird game. Consisted of an interactive menu, live score counter, saved high scores, and Flappy Bird game logic.",
     link: "https://github.com/DevionRosa/Flappy-Bird",
-    img: new URL("../assets/FlappyBirdimg.png", import.meta.url).href
+    img: new URL("../assets/FlappyBirdimg.png", import.meta.url).href,
+    tech: ["Python", "Pygame"],
   }
 };
 
 const ProjectList: React.FC = () => {
   return (
-    <div className="space-y-4 p-4 flex flex-col items-center">
+    <div className="space-y-6 p-4 flex flex-col items-center">
       {Object.keys(projects).map((key) => (
-        <div key={key} className="p-6 shadow w-3/4 mx-auto flex gap-6 hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+        <div 
+          key={key} 
+          className="p-6 bg-purple-900/20 border border-purple-800/50 rounded-xl shadow w-full lg:w-3/4 mx-auto flex flex-col md:flex-row gap-6 hover:shadow-[0_0_20px_rgba(168,85,247,0.2)] hover:-translate-y-1 transition-all duration-300"
+        >
           
-          {/* LEFT SIDE: The Image */}
-          <div className="flex-shrink-0">
+          {/* 1. LEFT SIDE: Image */}
+          <div className="flex-shrink-0 flex justify-center">
             <img
               src={projects[key].img}
               alt={projects[key].name}
-              
-              className="w-40 h-40 border-4 border-purple-950 rounded-md shadow object-cover"
+              className="w-40 h-40 border-2 border-purple-500 rounded-lg shadow-lg object-cover"
             />
           </div>
 
-          {/* RIGHT SIDE: The Text and Link */}
-          <div className="flex flex-col flex-grow">
-            {/* Project Name and Description grouped together */}
-            <h2 className="text-2xl font-bold">{projects[key].name}</h2>
-            <p className="mt-2 mb-4 flex-grow">
+          {/* 2. MIDDLE: Text Content */}
+          <div className="flex flex-col flex-grow min-w-0">
+            <h2 className="text-2xl font-bold text-white">{projects[key].name}</h2>
+            <p className="mt-2 text-gray-300 text-sm md:text-base line-clamp-3 md:line-clamp-none">
               {projects[key].description}
             </p>
             
-            {/* View Project Link/Button */}
-            {/* Added styling to make the link look more like a button */}
-            <div className="mt-auto">
+            <div className="mt-6 md:mt-auto">
               <a
                 href={projects[key].link}
-                className="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 hover:shadow-xl transition-colors duration-200"
+                className="inline-block px-6 py-2 bg-purple-600 text-white font-semibold rounded-lg hover:bg-purple-500 hover:shadow-lg transition-all duration-200"
               >
                 View Project
               </a>
+            </div>
+          </div>
+
+          {/* 3. RIGHT SIDE: Tech Stack Section */}
+          <div className="flex-shrink-0 w-full md:w-32 lg:w-40 border-t md:border-t-0 md:border-l border-purple-800 pt-4 md:pt-0 md:pl-6">
+            <h3 className="text-xs font-bold uppercase tracking-wider text-purple-400 mb-3 text-center md:text-left">
+              Tech Stack
+            </h3>
+            <div className="flex flex-wrap md:flex-col gap-2 justify-center md:justify-start">
+              {/* Assuming your projects object has a 'tech' array */}
+              {projects[key].tech?.map((techName: string) => (
+                <span 
+                  key={techName}
+                  className="px-2 py-1 bg-purple-950 border border-purple-500/30 text-purple-200 text-xs rounded font-mono text-center"
+                >
+                  {techName}
+                </span>
+              ))}
             </div>
           </div>
 
